ef38cec9e94381aaca2528cf0e32252120dffdff
iamsss <saurav9760@gmail.com>
2018-06-12 23:58:16 +0530

    Adding Cros For Cros Site Request

--- server/Server/config/view.config.js
+++ server/Server/config/view.config.js
@@ -2,13 +2,14 @@ const {app} = require('../server');
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
+var cors = require('cors')
 
 // Path Declarations
 const publicPath = path.join(__dirname, '../../Public');
 const partialViewPath = path.join(__dirname, '../Views/partials');
 const viewPath = path.join(__dirname, '\../Views');
 
-
+app.use(cors());
 app.use(express.static(publicPath));
 
 app.set('views', viewPath );  // Setting Default Path For Views
